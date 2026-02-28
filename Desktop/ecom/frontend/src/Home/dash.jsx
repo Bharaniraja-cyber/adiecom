2be722872e14fada6b113f1a6bd86ac52802704d
Bharaniraja-cyber <bharaniraja21@gmail.com>
2026-02-28 18:29:06 +0530

    first commit

--- Desktop/ecom/frontend/src/Home/dash.jsx
+++ Desktop/ecom/frontend/src/Home/dash.jsx
@@ -7,6 +7,7 @@ function Dash(){
     const [products, setProducts] = useState([]);
     const navigate = useNavigate();
     const API_URL = import.meta.env.VITE_API_URL;
+    
     useEffect(() => {
         axios.get(`${API_URL}/api/products`)
             .then(res => setProducts(res.data))
